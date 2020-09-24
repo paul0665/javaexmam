17d9e683cfe4d00c6f2e7c732b6fc67c057b0ffa
Paul <47947919+paul0665@users.noreply.github.com>
2020-09-24 09:45:02 +0800

    Add files via upload

---
+++ 20200924/helloworld1.java
@@ -0,0 +1,7 @@
+public class helloworld1{
+
+  public static void main(String[] argv){
+
+    System.out.println("My Java program.");
+  }
+}
---
+++ 20200924/helloworld2.java
@@ -0,0 +1,7 @@
+public class helloworld2{
+
+  public static void main(String[] argv){
+
+    System.out.println("     ************************\n     *    Java 程式設計     *\n     ************************");
+  }
+}
