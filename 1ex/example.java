bfea5f58b856a002b44d01d61ef577621535072d
admin <paul002706@gmail.com>
2020-10-15 11:16:42 +0800

    .

---
+++ 1ex/example.java
@@ -0,0 +1,10 @@
+public class example {
+  boolean aBoolean = false;
+  byte aByte = 0;
+  char aChar = 0;
+
+  public static void main(String[] args){
+
+    System.out.println("Hello world");
+  }
+}
--- 20201015/exam20201015_2.java
+++ 20201015/exam20201015_2.java
@@ -1,9 +1,14 @@
 public class exam20201015_2 {
 
-    static final double PI = 3.1416;
-
     public static void main(String[] args){
         float r1 = 3000f;
+        float r2 = 2000f;
+        float ans = 0f;
+        
+        ans = r1 + r2;
+        System.out.println("ans1 = " + ans);
+        ans = r1 * r2/ans;
+        System.out.println("ans1 = " + ans);
 
     }
 }
