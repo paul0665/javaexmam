398eaff36b324ad8113e7558521d963e9f38df3b
terry <>
2020-10-15 11:54:05 +0800

    .

--- 20201015/exam20201015_1.java
+++ 20201015/exam20201015_1.java
@@ -5,7 +5,7 @@ public class exam20201015_1 {
     public static void main(String[] args){
         byte b = -10;
         int i = 360;
-        long l = 2147438658;
+        long l = 2147483647L;
         float f = 1.6f;
         double d = -0.01d;
         String s = "SHU-TE University";
