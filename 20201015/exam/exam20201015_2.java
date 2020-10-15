4ef623bb8b62d106f27388a3376a32a10235a88b
admin <paul002706@gmail.com>
2020-10-15 11:45:14 +0800

    .

--- 20201015/exam/exam20201015_2.java
+++ 20201015/exam/exam20201015_2.java
@@ -1,10 +1,12 @@
+package exam;
+
 public class exam20201015_2 {
 
     public static void main(String[] args){
         float r1 = 3000f;
         float r2 = 2000f;
         float ans = 0f;
-        
+
         ans = r1 + r2;
         System.out.println("ans1 = " + ans);
         ans = r1 * r2/ans;
