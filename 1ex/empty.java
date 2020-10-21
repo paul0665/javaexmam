53338e7d967fe44687106717398a076e02be96f3
admin <paul002706@gmail.com>
2020-10-21 16:42:05 +0800

    .

--- 1ex/empty.java
+++ 1ex/empty.java
@@ -1,6 +1,11 @@
 public class empty {
 
     public static void main(String[] args){
+        byte b = -10;
+        int i = 360;
+        long l = 2147483647L;
+        float f = 1.6f;
+        double d = -0.01d;
         System.out.println("Hello world");
 
     }
