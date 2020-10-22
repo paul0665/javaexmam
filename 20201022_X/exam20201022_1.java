0c478eebb080105444a1fd0ef85a3289fc3df007
admin <paul002706@gmail.com>
2020-10-22 09:34:59 +0800

    .

--- 20201022_X/exam20201022_1.java
+++ 20201022_X/exam20201022_1.java
@@ -36,46 +36,5 @@ public class exam20201022_1 {
                 System.out.println("Enter 1");
             }
         }
-
-        /*
-        //10
-        if ((price % 10)!=0){
-            float i = price;
-            while (true){
-                if ((i % 10)==0){
-                    ans10 = i/10;
-                    break;
-                }
-                i++;
-            }
-        }else ans10=price;
-
-        //5
-        if ((price % 5)!=0){
-            float i = price;
-            while (true){
-                if ((i % 10)==0){
-                    ans5 = i/5;
-                    break;
-                }
-                i++;
-            }
-        }else ans5=price;
-
-        //1
-        if ((price % 1)!=0){
-            float i = price;
-            while (true){
-                if ((i % 1)==0){
-                    ans1 = i/1;
-                    break;
-                }
-                i++;
-            }
-        }else ans1=price;*/
-
-        //System.out.println("ans10 = " + ans10);
-        //System.out.println("ans5 = " + ans5);
-        //System.out.println("ans1 = " + ans1);
     }
 }
