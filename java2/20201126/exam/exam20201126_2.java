d9beb06e906c1f30e46e1d92595d0cac6c069264
admin <paul002706@gmail.com>
2020-11-26 11:15:32 +0800

    reform

--- java2/20201126/exam/exam20201126_2.java
+++ java2/20201126/exam/exam20201126_2.java
@@ -33,7 +33,7 @@ public class exam20201126_2 {
 
     public static void main(String[] args){
         for (int i=1; i<=h;i++) {
-            
+
             for (int l=0; l<(w-i);l++) {
                 System.out.print(" ");
             }
@@ -43,6 +43,5 @@ public class exam20201126_2 {
             }
             System.out.println();
         }
-
     }
 }
